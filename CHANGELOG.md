d089280fc6f1daec76b69f3147abdef2e1e8b020
UCSDMath <ucsdmath.project@gmail.com>
2015-12-10 09:49:57 -0800

    Update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,52 +1,1 @@
-CHANGELOG
-=========
-
-All notable changes to `UCSDMath\Pagination` (bug and security fixes) will
-be documented in this file.
-
-# 1.5.0 (2015-12-05)
-
- - Updated minor changes to `UCSDMath\DependencyInjection`
- - Updated minor SQL fixes
- - Updated coding changes anticipation of PHP7
- - Updated dependency to Symfony 3.0 (removed all 2.x)
- - Fixed Github commits for Mac OS X line-endings to Unix
- - Updated minor formatting and comments
- - Added minor changes to ready PHP7 phase
- - Various JavaScript updates
-
-# 1.4.0 (2015-10-12)
-
- - Updated DDL database schema for system_logs, office_hours, ta_assignments
- - Updated comments to Entity and Maps for use with Persistance class.
- - Various javascript updates
- - Added multiple validation methods found in Validation class
- - Corrected and updated method in Pagination
- - other items were done...
-
-# 1.3.0 (2015-09-29)
-
- - New use of ORM and class called Persistence
- - Created 38 new Entity and maps for testing database tables.
- - Added Doctrine ORM and connections using ConfigurationVault
- - Javascript updates
- - Updated some logging output for various applications
-
-# 1.2.0 (2015-09-10)
-
- - Updated core javascript
- - Added boolean AND as default for word spacing all applications
- - Updated AbstractDatabase to work with DDL/SQL Updates
- - Structural updates various
-
-# 1.1.0 (2015-08-07)
-
- - Updated core javascript
- - Added boolean AND as default for word spacing
- - Added new class component Pagination
- - Minor configuration changes
-
-# 1.0.5 (2015-07-25)
-
- - Initial Release of `UCSDMath\Pagination`
- - Initial Release version starts at v1.0.5 as introduced to the framework
+CHANGELOG=========All notable changes to `UCSDMath\Pagination` (bug and security fixes) willbe documented in this file.# 1.5.0 (2015-12-05) - Updated minor changes to `UCSDMath\DependencyInjection` - Updated minor SQL fixes - Updated coding changes anticipation of PHP7 - Updated dependency to Symfony 3.0 (removed all 2.x) - Fixed Github commits for Mac OS X line-endings to Unix - Updated minor formatting and comments - Added minor changes to ready PHP7 phase - Various JavaScript updates# 1.4.0 (2015-10-12) - Updated DDL database schema for system_logs, office_hours, ta_assignments - Updated comments to Entity and Maps for use with Persistance class. - Various javascript updates - Added multiple validation methods found in Validation class - Corrected and updated method in Pagination - other items were done...# 1.3.0 (2015-09-29) - New use of ORM and class called Persistence - Created 38 new Entity and maps for testing database tables. - Added Doctrine ORM and connections using ConfigurationVault - Javascript updates - Updated some logging output for various applications# 1.2.0 (2015-09-10) - Updated core javascript - Added boolean AND as default for word spacing all applications - Updated AbstractDatabase to work with DDL/SQL Updates - Structural updates various# 1.1.0 (2015-08-07) - Updated core javascript - Added boolean AND as default for word spacing - Added new class component Pagination - Minor configuration changes# 1.0.5 (2015-07-25) - Initial Release of `UCSDMath\Pagination` - Initial Release version starts at v1.0.5 as introduced to the framework
--- src/Pagination/Paginator.php
+++ src/Pagination/Paginator.php
@@ -1,95 +1,1 @@
-<?php
-
-/*
- * This file is part of the UCSDMath package.
- *
- * (c) UCSD Mathematics | Math Computing Support <mathhelp@math.ucsd.edu>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
-namespace UCSDMath\Pagination;
-
-/**
- * Pagination is the default implementation of {@link PaginationInterface} which
- * provides routine Paginator methods that are commonly used throughout the framework.
- *
- * Paginator provides a process of dividing (content) into discrete pages that are
- * acceptable or desirable to the enduser.
- *
- * Important considerations in writing this class are:
- *    - SEO Friendly URLS
- *    - Dynamic search results (sticky or hold state)
- *    - Standard scheme for Front Controllers
- *    - Provide options for template generator (e.g., Twig, Plates, Smarty)
- *    - Provided via a data structure (a raw data option)
- *
- * Technically, for pagination to work, all is needed is the page number of the current set.
- *
- *    $page = filter_input(INPUT_GET, 'page', FILTER_VALIDATE_INT);
- *    $itemsPerPage = 4;
- *
- *    $totalItems = "SELECT COUNT(*) FROM personnel;
- *    $rowCount   = "SELECT COUNT(*) FROM personnel where group = 'faculty';
- *
- *    if ($totalItems === 0) { print 'No records exist in the database.';}
- *    if ($rowCount === 0)   { print 'No records found in database with you exact match.';}
- *
- *    $pageCount = (int) ceil($rowCount / $itemsPerPage);
- *
- *    // range error; we could just set page = 1
- *    if ($page > $pageCount) {$page = 1;}
- *
- *    $offset = ($page - 1) * $itemsPerPage;
- *    $sql = "SELECT * FROM personnel where (group = 'faculty') (ORDER BY lastname, firstname) LIMIT " . $offset . "," . $itemsPerPage;
- *
- *    SQL looks like:  SELECT * FROM personnel LIMIT 4,4
- *
- * Consider some common url patterns:
- *    - /sso/1/personnel/(:page)/(:rows)/(:sort)/
- *    - /sso/1/personnel/quick-search/(:page)/(:rows)/(:search)/(:sort)/
- *    - /sso/1/personnel/edit-search/page-(:page)/show-(:rows)/(:search)/(:sort)/
- *    - /sso/1/personnel/edit-record/page-(:page)/
- *
- * Method list:
- *
- * The notation below illustrates visibility: (+) @api, (-) protected or private.
- *
- * @method PaginationInterface __construct();
- *
- * @author Daryl Eisner <deisner@ucsd.edu>
- *
- * @api
- */
-class Paginator extends AbstractPagination implements PaginationInterface
-{
-    /**
-     * Constants.
-     *
-     * @var string VERSION  A version number
-     *
-     * @api
-     */
-    const VERSION = '1.5.0';
-
-    // --------------------------------------------------------------------------
-
-    /**
-     * Properties.
-     */
-
-    // --------------------------------------------------------------------------
-
-    /**
-     * Constructor.
-     *
-     * @param array  $settings  A associated list of page settings.
-     *
-     * @api
-     */
-    public function __construct(array $settings = null)
-    {
-        parent::__construct($settings);
-    }
-}
+<?php/* * This file is part of the UCSDMath package. * * (c) UCSD Mathematics | Math Computing Support <mathhelp@math.ucsd.edu> * * For the full copyright and license information, please view the LICENSE * file that was distributed with this source code. */namespace UCSDMath\Pagination;/** * Pagination is the default implementation of {@link PaginationInterface} which * provides routine Paginator methods that are commonly used throughout the framework. * * Paginator provides a process of dividing (content) into discrete pages that are * acceptable or desirable to the enduser. * * Important considerations in writing this class are: *    - SEO Friendly URLS *    - Dynamic search results (sticky or hold state) *    - Standard scheme for Front Controllers *    - Provide options for template generator (e.g., Twig, Plates, Smarty) *    - Provided via a data structure (a raw data option) * * Technically, for pagination to work, all is needed is the page number of the current set. * *    $page = filter_input(INPUT_GET, 'page', FILTER_VALIDATE_INT); *    $itemsPerPage = 4; * *    $totalItems = "SELECT COUNT(*) FROM personnel; *    $rowCount   = "SELECT COUNT(*) FROM personnel where group = 'faculty'; * *    if ($totalItems === 0) { print 'No records exist in the database.';} *    if ($rowCount === 0)   { print 'No records found in database with you exact match.';} * *    $pageCount = (int) ceil($rowCount / $itemsPerPage); * *    // range error; we could just set page = 1 *    if ($page > $pageCount) {$page = 1;} * *    $offset = ($page - 1) * $itemsPerPage; *    $sql = "SELECT * FROM personnel where (group = 'faculty') (ORDER BY lastname, firstname) LIMIT " . $offset . "," . $itemsPerPage; * *    SQL looks like:  SELECT * FROM personnel LIMIT 4,4 * * Consider some common url patterns: *    - /sso/1/personnel/(:page)/(:rows)/(:sort)/ *    - /sso/1/personnel/quick-search/(:page)/(:rows)/(:search)/(:sort)/ *    - /sso/1/personnel/edit-search/page-(:page)/show-(:rows)/(:search)/(:sort)/ *    - /sso/1/personnel/edit-record/page-(:page)/ * * Method list: * * The notation below illustrates visibility: (+) @api, (-) protected or private. * * @method PaginationInterface __construct(); * * @author Daryl Eisner <deisner@ucsd.edu> * * @api */class Paginator extends AbstractPagination implements PaginationInterface{    /**     * Constants.     *     * @var string VERSION  A version number     *     * @api     */    const VERSION = '1.5.0';    // --------------------------------------------------------------------------    /**     * Properties.     */    // --------------------------------------------------------------------------    /**     * Constructor.     *     * @param array  $settings  A associated list of page settings.     *     * @api     */    public function __construct(array $settings = null)    {        parent::__construct($settings);    }}

84b5204d50fe63b5647faf973104b0061d2aab3f
Daryl Eisner <ucsdmath.project@gmail.com>
2016-08-22 17:10:39 -0700

    Update

--- src/Pagination/Paginator.php
+++ src/Pagination/Paginator.php
@@ -84,7 +84,7 @@ class Paginator extends AbstractPaginationOperations implements PaginationInterf
      *
      * @api
      */
-    const VERSION = '1.9.0';
+    const VERSION = '1.10.0';
 
     //--------------------------------------------------------------------------
 

94d007d2fe17460754bacf520a00eb4d17ee07d5
UCSDMath <UCSDMath.Project@gmail.com>
2017-02-14 10:50:05 -0800

    Update

--- src/Pagination/AbstractPaginationOperations.php
+++ src/Pagination/AbstractPaginationOperations.php
@@ -48,7 +48,7 @@ abstract class AbstractPaginationOperations extends AbstractPagination
      *
      * @api
      */
-    public const VERSION = '1.14.0';
+    public const VERSION = '1.15.0';
 
     //--------------------------------------------------------------------------
 

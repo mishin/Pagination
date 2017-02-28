7e64e4594b0a2cd26260148f3d6ed640f60fbd00
UCSDMath <UCSDMath.Project@gmail.com>
2017-02-27 18:22:36 -0800

    Update

--- src/Pagination/AbstractPagination.php
+++ src/Pagination/AbstractPagination.php
@@ -91,7 +91,7 @@ abstract class AbstractPagination implements PaginationInterface, ServiceFunctio
      *
      * @api
      */
-    public const VERSION = '1.15.0';
+    public const VERSION = '1.16.0';
 
     //--------------------------------------------------------------------------
 
--- src/Pagination/AbstractPaginationOperations.php
+++ src/Pagination/AbstractPaginationOperations.php
@@ -48,7 +48,7 @@ abstract class AbstractPaginationOperations extends AbstractPagination
      *
      * @api
      */
-    public const VERSION = '1.15.0';
+    public const VERSION = '1.16.0';
 
     //--------------------------------------------------------------------------
 
